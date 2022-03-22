a14d21f2452f301c8350cfa40febfb6e1e30cc9e
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-22 10:43:36 -0700

    setup routes, table, and test for creating an entry

---
+++ __tests__/author.test.js
@@ -0,0 +1,29 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('local-bookstore-backend routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('allows user to create an author entry', async () => {
+    const expected = {
+      name: 'George Orwell',
+      dob: '1950-01-25',
+      pob: 'Motihari, India'
+    };
+
+    const res = await request(app)
+      .post('/api/v1/authors')
+      .send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS authors; 
+
+CREATE TABLE authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    dob DATE NOT NULL,
+    pob TEXT NOT NULL
+);
+
+INSERT INTO
+    authors (name, dob, pob)
+VALUES
+    ('Hayao Miyazaki','1941-01-05','Tokyo, Japan'),
+    ('George Orwell','1950-01-25','Motihari, India'),
+    ('J.R.R Tolkien','1892-01-03','Bloemfontein, South Africa');
