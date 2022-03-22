f29a46b7265e5e98f2f254ad1d00d728afc291fc
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-22 11:08:37 -0700

    done with insert

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,21 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS publishers;
+
+CREATE TABLE publishers (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    country TEXT NOT NULL
+);
+
+INSERT INTO
+    publishers (name, city, state, country)
+VALUES
+    (
+        'hitRecords',
+        'portland',
+        'OR',
+        'United State'
+    )
