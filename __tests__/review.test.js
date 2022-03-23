47f05409dbbf703098eb75a850906cba025d4a18
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-22 17:56:24 -0700

    did stuff in reviews

---
+++ __tests__/review.test.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('local-bookstore-backend routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('creates a new review with correct book id and title', async () => {
+    const expected = {
+      id: expect.any(String),
+      rating: '5',
+      review: 'Great read',
+      bookId: '1',
+      bookTitle: expect.any(String),
+    };
+    const response = await request(app).post('/api/v1/reviews').send(expected);
+    expect(response.body).toEqual(expected);
+  });
+});
